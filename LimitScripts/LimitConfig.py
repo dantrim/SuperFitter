de8afac79222c6fcedbebea8d4e011b6c60e198c
Daniel Joseph Antrim <dantrim@uci.edu>
2015-09-25 06:42:55 -0700

    Update LimitConfig

--- LimitScripts/LimitConfig.py
+++ LimitScripts/LimitConfig.py
@@ -5,7 +5,7 @@
 ##                                               ##
 ##          HistFitter configuration             ##
 ##                                               ##
-##           http://bit.ly/1KTbAoV               ##
+##           https://xkcd.com/882/               ##
 ##                                               ##
 ## --------------------------------------------- ##
 ##  daniel antrim                                ##
@@ -13,6 +13,14 @@
 ##  September 2015                               ##
 ###################################################
 """
+
+### ROOT imports
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(True)
+from ROOT import kBlack,kWhite,kGray,kRed,kPink,kMagenta,kViolet,kBlue,kAzure,kCyan,kTeal,kGreen,kSpring,kYellow,kOrange 
+from ROOT import gROOT
+
 ### histfitter imports
 from configManager import configMgr
 from configWriter import fitConfig,Measurement,Channel,Sample
@@ -25,27 +33,31 @@ import sys
 from optparse import OptionParser
 
 
-### ROOT imports
-from ROOT import kBlack,kWhite,kGray,kRed,kPink,kMagenta,kViolet,kBlue,kAzure,kCyan,kTeal,kGreen,kSpring,kYellow,kOrange 
-from ROOT import gROOT
-import ROOT
 
 ### SuperFitter imports
 sys.path.append(os.environ['LIMITDIR'])
 import RegionDefs # this is where all of the selections are defined
-
-regContainer = RegionDefs.buildRegions()
-regContainer.Print()
-cutdict = regContainer.getDict()
-print cutdict
+import GridDefs # this is where the handling of the signal grid is performed
 
 #####################################################################
 ### parse the user input
 parser = OptionParser()
 parser.add_option("-r", "--signalRegion", dest="signalRegion", default="")
+parser.add_option("-g", "--sigGrid",   dest="sigGrid", default="")
 parser.add_option("-t", "--doTheoryBand", action="store_true", dest="doTheoryBand", default=False)
 (options, args) = parser.parse_args()
 
+
+##########################################
+## get available region definitions
+regContainer = RegionDefs.buildRegions()
+regContainer.Print()
+cutdict = regContainer.getDict()
+print cutdict
+
+verbose = True # hardcode this for now
+
+##########################################
 ## take in the signal region choice
 if str(options.signalRegion) not in cutdict.keys() :
     print 'SuperFitter LimitConfig ERROR    Requested signal region "%s" not supported. Check RegionDefs.buildRegions()!'%options.signalRegion
@@ -53,6 +65,13 @@ if str(options.signalRegion) not in cutdict.keys() :
     sys.exit()
 signalRegion = options.signalRegion
 
+##########################################
+## construct the signal grid 
+gridname = options.sigGrid
+signalGrid = GridDefs.SignalGrid(gridname, verbose)
+print signalGrid.getSampleList()
+
+##########################################
 ## do we want to run the theory XS band
 doTheoryBand = options.doTheoryBand
 
